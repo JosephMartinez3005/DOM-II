4db7d00f7492e1c6a19c1f6dec21b406fd55ffec
JosephMartinez3005 <52721764+JosephMartinez3005@users.noreply.github.com>
2021-01-17 19:30:38 -0800

    Added two Events

--- js/index.js
+++ js/index.js
@@ -1,2 +1,27 @@
-const h1 = document.querySelector('.logo-heading');
-h1.style.fontsize = '320rem'
+const nav = document.querySelector('.nav')
+nav.style.textAlign = 'center'
+nav.style.fontSize = '1.5rem'
+
+//selecting the h1. 
+
+let h1 = document.querySelector('h1');
+//change h1 color on hover.
+function onH1Hover(event){
+    h1.style.fontSize = '7rem';
+    h1.style.transition = '350ms'
+    h1.style.color='orange';
+}
+
+h1.addEventListener('mouseover', onH1Hover)
+//page style
+h1.style.fontSize = '5rem';
+h1.style.textAlign ='center';
+
+//change h1 Color on mouse out
+function changeH1Color (event){
+    h1.style.color ='black'
+    h1.style.fontSize ='5rem'
+    h1.style.transition = '350ms'
+}
+h1.addEventListener('mouseout', changeH1Color)
+
